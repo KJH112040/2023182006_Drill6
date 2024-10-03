8b0e30e03d5609f4f5ef1c56256882a4d2136cdb
KJH04_ <10wjdgp04@gmail.com>
2024-10-03 11:02:05 +0900

    배경 띄우기

--- go_to_arrow.py
+++ go_to_arrow.py
@@ -1,7 +1,27 @@
 from pico2d import*
 
 open_canvas()
+
 tuk = load_image('TUK_GROUND.png')
 boy = load_image('animation_sheet.png')
+arrow = load_image('hand_arrow.png')
+
+def handle_events():
+    global run
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            run = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                run = False
+
+run = True
+
+while run:
+    clear_canvas()
+    tuk.draw(400,300,800,600)
+    update_canvas()
+    handle_events()
 
 close_canvas()

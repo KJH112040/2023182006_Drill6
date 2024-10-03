e82288f954c2ecba7d780ed06a153d4617fefab5
KJH04_ <10wjdgp04@gmail.com>
2024-10-03 11:39:48 +0900

    화살표 위치 랜덤 생성, 소년과 화살표 이미지 띄우기

--- go_to_arrow.py
+++ go_to_arrow.py
@@ -1,4 +1,5 @@
 from pico2d import*
+import random
 
 open_canvas()
 
@@ -16,12 +17,29 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 run = False
 
+def random_move_arrow():
+    global arrow_x, arrow_y, goal
+    arrow_x, arrow_y = random.randrange(50,750),random.randrange(52,548)
+    goal = False
+
 run = True
+frame_x = 0
+dir = 3
+
+random_move_arrow()
 
 while run:
     clear_canvas()
     tuk.draw(400,300,800,600)
+    boy.clip_draw(frame_x*802//8,dir*402//4,802//8,402//4,400,300,90,90)
+    arrow.draw(arrow_x,arrow_y,50,52)
     update_canvas()
     handle_events()
+    if goal:
+        random_move_arrow()
+    else:
+        a=0
+        
+    delay(0.05)
 
 close_canvas()
